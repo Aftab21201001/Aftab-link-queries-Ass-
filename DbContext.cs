e403a4b13baeb0d4ee9cc362f28b8977b084a8d6
Aftab21201001 <106833639+Aftab21201001@users.noreply.github.com>
2023-12-13 01:32:31 +0500

    Add files via upload

---
+++ DbContext.cs
@@ -0,0 +1,44 @@
+﻿using _017_web_Ass2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _017_web_Ass2.Context
+{
+    public class DbContext
+    {
+        
+   public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ModelClasses> Students { get; set; }
+        public DbSet<ModelClasses> Classs { get; set; }
+        public DbSet<ModelClasses> Facultys { get; set; }
+        public DbSet<ModelClasses> Enrolleds { get; set; }
+    }
+
+   
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Define relationships
+
+        modelBuilder.Entity<Enrolled>()
+            .HasKey(e => e.Id);
+
+        modelBuilder.Entity<Enrolled>()
+            .HasOne(e => e.Student)
+            .WithMany(s => s.Enrollments)
+            .HasForeignKey(e => e.Sid);
+
+        modelBuilder.Entity<Enrolled>()
+            .HasOne(e => e.Class)
+            .WithMany(c => c.Enrollments)
+            .HasForeignKey(e => e.Cid);
+
+        modelBuilder.Entity<Class>()
+            .HasOne(c => c.Faculty)
+            .WithMany(f => f.Classes)
+            .HasForeignKey(c => c.Fid);
+    }
+}
+}
---
+++ ModelClasses.cs
@@ -0,0 +1,46 @@
+﻿namespace _017_web_Ass2.Models
+{
+    public class ModelClasses
+    {
+        public class Student
+        {
+            public int Sid { get; set; }
+            public string Sname { get; set; }
+            public string Major { get; set; }
+            public string Standing { get; set; }
+
+            public List<Enrolled> Enrollments { get; set; }
+        }
+
+        public class Faculty
+        {
+            public int Fid { get; set; }
+            public string Fname { get; set; }
+            public int Deptid { get; set; }
+            public string Standing { get; set; }
+
+            public List<Class> Classes { get; set; }
+        }
+
+        public class Class
+        {
+            public int Cid { get; set; }
+            public string Name { get; set; }
+            public string RoomNumber { get; set; }
+            public int Fid { get; set; }
+
+            public Faculty Faculty { get; set; }
+            public List<Enrolled> Enrollments { get; set; }
+        }
+
+        public class Enrolled
+        {
+            public int Id { get; set; }
+            public int Sid { get; set; }
+            public int Cid { get; set; }
+
+            public ModelClasses Student { get; set; }
+            public Class Class { get; set; }
+        }
+    }
+}
